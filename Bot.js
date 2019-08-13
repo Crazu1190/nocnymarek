bfe8c44d975d5f66a817001e13d93514bb36ef6e
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-13 19:44:15 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -5,5 +5,22 @@ client.on('ready', () => {
 
 });
 
+client.on('message', message => {
+    if (message.content === '!konkurs') {
+    	message.channel.send('@everyone');
+        message.channel.send('Konkurs na mem miesiąca.');
+        message.channel.send('■■■■■■■■■■■■');
+        message.channel.send('Co trzeba zrobić aby wygrać?');
+        message.channel.send('**Zrobić śmieszny mem**');
+        message.channel.send('Wymagania:');
+        message.channel.send('**Dołączyć na serwer discord:**');
+        message.channel.send('https://discord.gg/StJHcnV');
+        message.channel.send('**Nagrody;**');
+        message.channel.send('***Rzadka ranga***');
+        message.channel.send('**10k waluty serwerowej**');
+        message.channel.send('**Emotki do nicku.**');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
