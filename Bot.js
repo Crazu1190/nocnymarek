ead2a9c58dbdb148f7039bcba09c3805016f5bb2
Crazu1190 <53394381+Crazu1190@users.noreply.github.com>
2019-08-13 20:16:51 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -6,13 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '!konjsnsnsbsbs') {
-        message.channel.send('<@&609160113265311787> Konkurs na mem miesiąca.');
-        message.channel.send('**Najzabawniejszy mem wygrywa!**');
-        message.channel.send('**Nagrody:**');
-        message.channel.send('***Rzadka ranga***');
-        message.channel.send('**10k waluty serwerowej**');
-        message.channel.send('**Twój mem będzie pod !mem**');
+    if (message.content === '<@&610877604173709312>') {
+        message.reply('zostaw mnie!');
   	}
 });
 
